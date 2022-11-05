df2263200e52a3aea7e6c7524dce992fa0008cec
medofayed <mfedo32@gmail.com>
2022-11-05 05:29:07 +0200

    Added user registration - ch6

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,25 @@
-# mysite/urls.py
+# mfd_site/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("pages.urls")),
+	# Django admin
+	path("admin/", admin.site.urls),
+	# User management
+	path("accounts/", include("django.contrib.auth.urls")), # new
+	# Local apps
+    path("accounts/", include("accounts.urls")), # new page 101
+	path("", include("pages.urls")),
 ]
+
+"""
+# these are automatically included
+accounts/login/ [name="login"]
+accounts/logout/ [name="logout"]
+accounts/password_change/ [name="password_change"]
+accounts/password_change/done/ [name="password_change_done"]
+accounts/password_reset/ [name="password_reset"]
+accounts/password_reset/done/ [name="password_reset_done"]
+accounts/reset/<uidb64>/<token>/ [name="password_reset_confirm"]
+accounts/reset/done/ [name="password_reset_complete"]
+"""
